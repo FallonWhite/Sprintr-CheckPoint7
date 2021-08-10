57739d5624fb55dc7d93cd1f1ad2bf3cd1bb400f
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-08-10 13:10:49 -0600

    fix

--- Sprintr-CheckPoint7/server/controllers/ProjectsController.js
+++ Sprintr-CheckPoint7/server/controllers/ProjectsController.js
@@ -1,14 +1,13 @@
-import {projectsService} from '../services/ProjectsService'
+import { projectsService } from '../services/ProjectsService'
 import BaseController from '../utils/BaseController'
 // @ts-ignore
 import { Auth0Provider } from '@bcwdev/auth0provider'
-
 
 export class ProjectsController extends BaseController {
   constructor() {
     super()
     this.router
-    .use(Auth0Provider.getAuthorizedUserInfo)
+      .use(Auth0Provider.getAuthorizedUserInfo)
       .get('', this.getAllProjects)
       .get('/:id', this.getSingleProject)
       .post('', this.createProject)
@@ -18,12 +17,13 @@ export class ProjectsController extends BaseController {
 
   async getAllProjects(req, res, next) {
     try {
-      const projects = await projectsService.getAllProjects({creatorid: req.userInfo.id})// do I need to put anything in paranthises?
+      const projects = await projectsService.getAllProjects({ creatorid: req.userInfo.id })// do I need to put anything in paranthises?
       res.send(projects)
     } catch (error) {
       next(error)
     }
   }
+
   async getSingleProject(req, res, next) {
     try {
       // clearification on req.params.id, req.id?
@@ -36,30 +36,31 @@ export class ProjectsController extends BaseController {
 
   async createProject(req, res, next) {
     try {
-      // what dose below relation defines? 
-      req.body.creatorId=req.params.id
+      // what dose below relation defines?
+      req.body.creatorId = req.params.id
       const project = await projectsService.createProject(req.body) // why req.body?
       res.send(project)
     } catch (error) {
       next(error)
     }
   }
- async updateProject(req, res, next) {
-  try {
-    req.body.id = req.params.id
-    const project = await projectsService.updateProject(req.body)
-    res.send(project)
-  } catch (error) {
-    next(error)
+
+  async updateProject(req, res, next) {
+    try {
+      req.body.id = req.params.id
+      const project = await projectsService.updateProject(req.body)
+      res.send(project)
+    } catch (error) {
+      next(error)
+    }
   }
-  }
+
   async destroyProject(req, res, next) {
-  try {
-    await projectsService.destroyProject(req.body)
-    res.send({message: 'You deleted'})
-  } catch (error) {
-    next(error)
-    
-  }
+    try {
+      await projectsService.destroyProject(req.params.id)
+      res.send({ message: 'You deleted' })
+    } catch (error) {
+      next(error)
+    }
   }
 }
