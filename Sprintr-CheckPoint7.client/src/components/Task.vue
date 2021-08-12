25331112ab9d7ec3c4f58d66be3907262f7225d1
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-08-12 16:50:58 -0600

    task

---
+++ Sprintr-CheckPoint7.client/src/components/Task.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="component">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Component',
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
