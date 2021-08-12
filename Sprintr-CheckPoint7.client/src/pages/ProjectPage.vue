74c710a97683e1eaac5621375a681d691ba2269c
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-08-12 06:52:15 -0600

    CardsModalPage.Error404OnProjectPage(Project)

--- Sprintr-CheckPoint7.client/src/pages/ProjectPage.vue
+++ Sprintr-CheckPoint7.client/src/pages/ProjectPage.vue
@@ -1,13 +1,52 @@
 <template>
-  <div class="project">
-    <h1 class="text-info p-3 pb-1 m-1" style="text-align: center; text-shadow: 2px 2px black; background-color: brown">
-      This is the project page
-    </h1>
+  <div class="project container-fluid ">
+    <div class="row mt-4">
+      <div class="col-12">
+        <h1 class="text-info p-3 pb-1 m-1" style="text-align: center; text-shadow: 2px 2px black; background-color: brown">
+          <button class="btn btn-info text-center mr-4" data-target="#project-modal" data-toggle="modal">
+            <b>Create Project</b>
+          </button>
+          Welcome to Your Project Page
+          <button class="btn btn-info text-center ml-4" data-target="#task-modal" data-toggle="modal">
+            <b>Create Task</b>
+          </button>
+        </h1>
+      </div>
+    </div>
+    <div class="row">
+      <ProjectCard v-for="p in projects" :key="p.id" :project-prop="p" />
+      <!-- <TaskCard v-for="t in tasks" :key="t.id" :task-prop="t" /> -->
+    </div>
   </div>
 </template>
 
 <script>
+import { computed, onMounted } from '@vue/runtime-core'
+import { projectsService } from '../services/ProjectsService'
+// import { tasksService } from '../services/TasksService'
+import Pop from '../utils/Notifier'
+import { AppState } from '../AppState'
+import ProjectCard from '../components/ProjectCard.vue'
+// import TaskCard from '../components/TaskCard.vue'
 export default {
-  name: 'ProjectPage'
+  name: 'Home',
+  setup() {
+    onMounted(async() => {
+      try {
+        await projectsService.getAll()
+        // await tasksService.getById(id)
+      } catch (error) {
+        Pop.toast(error, 'error')
+      }
+    })
+    return {
+      projects: computed(() => AppState.projects)
+      // tasks: computed(() => AppState.tasks)
+    }
+  },
+  components: {
+    ProjectCard
+    // TaskCard
+  }
 }
 </script>
