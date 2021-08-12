5320ca01598bff390afa54c470de627a977bf3aa
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-08-11 23:19:29 -0600

    PageFix

--- Sprintr-CheckPoint7.client/src/pages/ProjectPage.vue
+++ Sprintr-CheckPoint7.client/src/pages/ProjectPage.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="project">
-    <h1>This is the project page</h1>
+    <h1 class="text-info p-2 ml-1" style="text-shadow: 2px 2px black">
+      This is the project page
+    </h1>
   </div>
 </template>
 
