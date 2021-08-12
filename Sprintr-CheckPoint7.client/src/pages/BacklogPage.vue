3295374e6469d587ea5213af21b92da4b44278ce
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-08-12 14:58:29 -0600

    Backlog and Sprint page

---
+++ Sprintr-CheckPoint7.client/src/pages/BacklogPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+    <div class="col-12" v-for="b in backlogs" :key="b.id">
+      <Backlog :backlog="b" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+import { AppState } from '../AppState'
+export default {
+  setup() {
+    return {
+      backlogs: computed(() => AppState.backlogs)
+    }
+  }
+}
+</script>>
