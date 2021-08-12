f9a73ed675a1c731faa0d0889c3fd6c79c080413
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-08-11 23:25:09 -0600

    pageStyle

--- Sprintr-CheckPoint7.client/src/pages/ProjectPage.vue
+++ Sprintr-CheckPoint7.client/src/pages/ProjectPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="project">
-    <h1 class="text-info p-2 ml-1" style="text-shadow: 2px 2px black">
+    <h1 class="text-info p-3 pb-1 m-1" style="text-align: center; text-shadow: 2px 2px black; background-color: brown">
       This is the project page
     </h1>
   </div>
