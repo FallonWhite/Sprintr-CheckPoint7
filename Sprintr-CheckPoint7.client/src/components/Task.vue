5dc1178f1d7b92ee253f03439efc62e36ae995c5
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-08-12 23:20:51 -0600

    Task component completed

--- Sprintr-CheckPoint7.client/src/components/Task.vue
+++ Sprintr-CheckPoint7.client/src/components/Task.vue
@@ -1,17 +1,89 @@
+
 <template>
-  <div class="component">
+  <div class="modal fade" :id="'create'+ btask.id" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">
+            Add Task
+          </h5>
+          <button type="button" class="btn-close btn btn-outline-danger" data-dismiss="modal" aria-label="Close">
+            Delete
+          </button>
+        </div>
+        <div class="modal-body">
+          <input
+            class="form-control"
+            type="text"
+            v-model="state.newTask.description"
+            id="description"
+            placeholder="Task description..."
+          >
+          <br>
+          <label for="weight" class="text-dark">Weight</label>
+          <input
+            class="form-control"
+            type="number"
+            v-model="state.newTask.weight"
+            id="weight"
+            max="6"
+            min="1"
+            placeholder="1"
+          >
+        </div>
+        <div class="modal-footer">
+          <button type="button" @click.stop="create" class="btn btn-primary" data-toggle="modal" data-target="#create">
+            Create Tasks
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { reactive } from '@vue/reactivity'
+import { tasksService } from '../services/TasksService'
+import Pop from '../utils/Notifier'
+import { computed } from '@vue/runtime-core'
+import { AppState } from '../AppState'
+import { useRoute, useRouter } from 'vue-router'
+import $ from 'jquery'
 export default {
   name: 'Component',
-  setup() {
-    return {}
-  }
+  props: {
+    btask: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    const router = useRouter()
+    const route = useRoute()
+    const state = reactive({
+      newTask: {
+        weight: 0
+      }
+    })
+    return {
+      state,
+      route,
+      router,
+      account: computed(() => AppState.account),
+      tasks: computed(() => AppState.tasks),
+      async createTask() {
+        try {
+          state.newTask.projectId = route.params.id
+          state.newTask.backlogId = props.btask.id
+          await tasksService.create(state.newTask)
+          state.newTask = {}
+          Pop.toast('Task created successfuly')
+        } catch (error) {
+          Pop.toast(error, 'error')
+        }
+      }
+    }
+  },
+  components: {}
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
