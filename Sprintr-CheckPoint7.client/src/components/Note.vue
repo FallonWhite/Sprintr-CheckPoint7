0536b7b81c77f69f2f182bbccb3f1174416dbbd8
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-08-12 21:48:57 -0600

    Notes Template structured

--- Sprintr-CheckPoint7.client/src/components/Note.vue
+++ Sprintr-CheckPoint7.client/src/components/Note.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="component">
+  <div class="modal fade" id="create" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">
+            Create
+          </h5>
+          <button type="button" class="btn-close btn btn-outline-danger" data-dismiss="modal" aria-label="Close">
+            Delete
+          </button>
+        </div>
+        <div class="modal-body">
+          <!-- VModel -->
+          <input
+            class="form-control"
+            type="text"
+            v-model="state.newNote.name"
+            id="name"
+            placeholder="Name Note..."
+          >
+          <br>
+          <textarea
+            class="form-control"
+            id="description"
+            v-model="state.newNote.description"
+            rows="6"
+            placeholder="Description..."
+          >
+          </textarea>
+        </div>
+        <div class="modal-footer">
+          <button type="submit" @click="create" class="btn btn-primary" data-toggle="modal" data-target="#create">
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
