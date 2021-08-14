e0873eb5d35e21674d974ed4c68db80d9df71416
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-08-13 22:14:28 -0600

    TaskDetals initalized

--- Sprintr-CheckPoint7.client/src/components/TaskModal.vue
+++ Sprintr-CheckPoint7.client/src/components/TaskModal.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="component">
+  <div class="modal fade"
+       :id="'createTaskModal' + backlog.id"
+       tabindex="-1"
+       role="dialog"
+       aria-labelledby="modelTitleId"
+       aria-hidden="true"
+  >
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">
+            Create Task
+          </h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
